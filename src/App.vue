a98a9d0e055809f4713602910b370b6cf515ef41
Ilya Podolyak <bobandmermaid@yandex.com>
2021-01-17 20:21:43 +0300

    add Data

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,17 @@
 <template>
   <div id="app">
-    <div class='table'>
-      <UserPopup
-        :tableData = 'tableData'
-        @addUser = 'saveUser($event)'
-      />
-      <UsersList
-        :tableData = 'tableDataChildren'
-      />
-    </div>
+    <Data value />
   </div>
 </template>
 
 <script>
-import UserPopup from './components/UserPopup';
-import UsersList from './components/UsersList';
+import Data from './components/Data';
 
 export default {
   name: 'App',
   components: {
-    UsersList,
-    UserPopup
+    Data
   },
-  data() {
-    return {
-      tableData: []
-    };
-  },
-  created() {
-    this.getList();
-  },
-  methods: {
-    saveUser(data) {
-      const chief = this.tableData.find(user => user.id === data.chiefId);
-      const result = {
-        id: this.tableData.length + 1,
-        name: data.name,
-        phone: data.phone,
-        parentId: chief ? chief.id : null,
-        level: chief ? chief.level + 1 : 1
-      };
-
-      this.tableData.push(result);
-      localStorage.setItem('tableData', JSON.stringify(this.tableData));
-    },
-    getList() {
-      this.tableData = JSON.parse(localStorage.getItem('tableData')) || [];
-    },
-  },
-  computed: {
-    tableDataChildren() {
-      const tableData = this.tableData
-        .map((elem) => Object.assign(elem, { children: [] }))
-        .sort((a, b) => (a.level < b.level ? 1 : -1));
-
-      const res = tableData.slice();
-
-      tableData.forEach((user) => {
-        res.forEach((item) => {
-          if (user.parentId === item.id) {
-            item.children.push(user);
-          }
-        });
-      });
-
-      return res.filter((node) => node.level === 1);
-    }
-  }
 }
 </script>
 
@@ -90,10 +35,5 @@ export default {
 *:after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.table {
-  margin: 150px auto;
-  width: 400px;
 }
 </style>
